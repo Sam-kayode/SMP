b77e4e86d50ee69592cebf89109250036258dc46
Sam-kayode <marvelloussamkayode@gmail.com>
2022-06-13 18:32:51 +0100

    first smp commit

---
+++ layouts/default.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="body"> 
+    <img src="~/static/images/shape1.svg" alt="" class="blob-1">
+    <Nuxt/>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+body{
+
+.blob-1{
+  left:-140px;
+  top:-140px;
+  position:fixed;
+  z-index:-3;
+}
+}
+
+
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+<div>
+  Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa autem, ab debitis et necessitatibus incidunt repudiandae quidem! Eos amet qui recusandae similique ex maiores voluptatem reprehenderit, voluptas praesentium voluptate minima.
+</div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+}
+</script>
+
+<style scoped lang="scss">
+
+
+
+</style>
