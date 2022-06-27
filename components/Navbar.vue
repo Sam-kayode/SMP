d5b8b25eac3abf6ed779306affaedf12e3573755
Sam-kayode <marvelloussamkayode@gmail.com>
2022-06-27 01:22:30 +0100

    add hamburger component

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,16 +1,19 @@
 <template>
-  <div class="navbar" :class="{ 'nav-position': windowTop > 180 }">
-    <div class="logo-container">
-      <img src="~/static/images/logo.svg" alt="" class="logo" />
-      <img src="~/static/images/smp.svg" alt="" class="smp" />
+  <div class="navbar-contain">
+    <div class="navbar" :class="{ 'nav-position': windowTop > 180 }">
+      <div class="logo-container">
+        <img src="~/static/images/logo.svg" alt="" class="logo" />
+        <img src="~/static/images/smp.svg" alt="" class="smp" />
+      </div>
+      <ul>
+        <li><a href="">Home</a></li>
+        <li><a href="">Works</a></li>
+        <li><a href="">Services</a></li>
+        <li><a href="">Get in touch</a></li>
+        <li><a href="">Portfolio</a></li>
+      </ul>
+      <div><Hamburger /></div>
     </div>
-    <ul>
-      <li><a href="">Home</a></li>
-      <li><a href="">Works</a></li>
-      <li><a href="">Services</a></li>
-      <li><a href="">Get in touch</a></li>
-      <li><a href="">Portfolio</a></li>
-    </ul>
   </div>
 </template>
 
@@ -42,6 +45,7 @@ export default {
   width: 100vw;
   padding: 10px 6%;
   display: flex;
+  flex-direction: row;
   align-items: center;
   height: 80px;
   z-index: 1000;
@@ -53,7 +57,7 @@ export default {
     height: 60px;
   }
   @include mobile {
-    display: block;
+    display:flex;
     margin: auto;
     height: 50px;
   }
@@ -61,38 +65,45 @@ export default {
   .logo {
     height: 100%;
     display: inline;
+    position: relative;
+    z-index: 10;
   }
 
   .smp {
     margin-left: -5px;
     width: 45px;
+    position: relative;
+    z-index: 10;
   }
 
   ul {
     list-style: none;
     margin: 0;
+
     @include mobile {
       position: fixed;
       z-index: 5;
-      background: rgb(252, 199, 254);
+      background: rgb(255, 255, 255);
       width: 100vw;
       height: 100vh;
-      top: 0;
+      top: 0px;
       left: 0;
       display: flex;
       flex-direction: column;
       justify-content: center;
       align-items: center;
+
       display: none;
     }
 
     li {
       display: inline;
       padding: 0px 1.5vw;
+      font-size: 18px;
+
       a {
         color: rgb(109, 91, 161);
         font-family: 'open sans', sans-serif;
-        font-size: 1.1rem;
       }
       @include mobile {
         display: block;
@@ -100,6 +111,8 @@ export default {
         text-align: center;
         margin: 20px auto;
         padding: 0;
+        font-size: 30px;
+        font-weight: bolder;
       }
     }
   }
