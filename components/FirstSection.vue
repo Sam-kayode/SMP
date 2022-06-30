ae53a7a7feaa4f24df62d8ec5e2cae9b8f97dc7e
Sam-kayode <marvelloussamkayode@gmail.com>
2022-06-30 02:24:17 +0100

    add index page components

---
+++ components/FirstSection.vue
@@ -0,0 +1,124 @@
+<template>
+  <div class="first">
+    <div class="left">
+      <h4>Hi There, I am</h4>
+      <h1 class="p-0 m-0">Tum<span>il</span>ara</h1>
+      <h3 class=""><span>-</span> The Social Media Princess <span>-</span></h3>
+      <p>
+        I make your audience my <span>PRIORITY</span> by
+        <span>INFLUENCING </span>them through
+        <span>YOU</span>
+      </p>
+    </div>
+    <div class="right">
+      <img
+        src="https://res.cloudinary.com/sam-kay/image/upload/v1655297755/smp/Group_1_1_2_mxfflk.png"
+        alt=""
+        class="tumi"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+.first {
+  display: grid;
+  grid-template-columns: 1fr 1.3fr;
+  align-items: center;
+  width: 100%;
+  padding: 0px 10%;
+
+  @include mobile {
+    grid-template-columns: 1fr;
+  }
+  @include bg-tablet {
+    padding: 0px 5%;
+  }
+  .right {
+    .tumi {
+      width: 100% !important;
+      height: auto;
+      padding: 5px;
+      overflow-x: hidden;
+    }
+    @include tablet {
+      order: 1;
+      margin-top: 50px;
+    }
+  }
+  .left {
+    @include tablet {
+      order: 1;
+      margin-top: 50px;
+    }
+    h4 {
+      font-family: 'open sans', sans-serif;
+      font-size: 16px;
+      font-weight: bolder;
+
+      @include bg-tablet {
+        font-size: 16px;
+      }
+    }
+
+    h1 {
+      font-family: 'josefin sans', sans-serif;
+      font-size: 620%;
+      font-weight: bolder;
+
+      @include mobile {
+        font-size: 40px;
+      }
+
+      @include bg-tablet {
+        font-size: 70px;
+      }
+
+      span {
+        color: #a966c8;
+      }
+    }
+    h3 {
+      font-family: 'open sans', sans-serif;
+      font-size: 20px;
+      margin-bottom: 35px;
+      margin-top: -10px;
+      font-weight: bolder;
+      color: #a966c8;
+
+      @include mobile {
+        font-size: 12px;
+      }
+
+      @include bg-tablet {
+        font-size: 16px;
+      }
+    }
+    p {
+      font-family: 'open sans', sans-serif;
+      font-size: 18px;
+      color: #000000;
+      width: 85%;
+
+      span {
+        // font-size: 1.7vw;
+        color: #a966c8;
+        font-weight: bolder;
+      }
+
+      @include mobile {
+        font-size: 16px;
+        width: 90%;
+      }
+
+      @include bg-tablet {
+        font-size: 16px;
+      }
+    }
+  }
+}
+</style>
---
+++ components/SecondSection.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="second">
+    <div class="left">
+      <img
+        src="https://res.cloudinary.com/sam-kay/image/upload/v1655414741/smp/picture1_oea911.png"
+        alt=""
+      />
+    </div>
+    <div class="right">
+      <div class="content">
+        <div class="content p-0 mt-5 mt-md-0">
+          <h2>Keep your audience <span>addicted</span> to your content</h2>
+          <p>
+            With me, you have absolutely nothing to worry as the best social
+            media needs any business owner can ask for are met. Stay organized,
+            save time and easily manage your social media presence without any
+            hassle.
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+.second {
+  margin-top: 150px;
+  display: grid;
+  grid-template-columns: 1fr 1.2fr;
+  align-items: center;
+  width: 100%;
+  padding: 0px 10%;
+
+  @include bg-tablet {
+    margin-top: 14vh;
+    padding: 0px 5%;
+  }
+  @include tablet {
+    grid-template-columns: 1fr 1fr;
+  }
+  @include mobile {
+    grid-template-columns: 1fr;
+  }
+
+  .left {
+    img {
+      max-width: 100%;
+    }
+  }
+  .right {
+    .content {
+      padding: 0 0 0 60px;
+
+      @include mobile {
+        padding: 0;
+      }
+
+      h2 {
+        font-family: 'josefin sans', sans-serif;
+        font-size: 64px;
+        font-weight: bolder;
+        letter-spacing: 0.1px;
+
+        @include bg-tablet {
+          font-size: 36px;
+        }
+        @include mobile {
+          font-size: 32px;
+        }
+
+        span {
+          color: #a966c8;
+        }
+      }
+
+      p {
+        font-family: 'open sans', sans-serif;
+        font-size: 18px;
+        color: #000000;
+        @include tablet {
+          font-size: 16px;
+        }
+      }
+    }
+  }
+}
+</style>
