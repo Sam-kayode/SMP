3a452216df4e8622a471f522fc70cb35458a0804
Sam-kayode <marvelloussamkayode@gmail.com>
2022-07-12 23:55:15 +0100

    add spalshScreen style fix

--- components/SplashScreen.vue
+++ components/SplashScreen.vue
@@ -188,7 +188,7 @@ export default {
   position: absolute;
   top: 50%;
   left: 50%;
-  transform: translate(-50%, -50%);
+  transform: translate(-50%, -55%);
   width: 100vw;
   overflow: clip;
   text-align: center;
