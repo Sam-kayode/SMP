e82b7873aa139010f13bcc67387d007aabb27ed8
Sam-kayode <marvelloussamkayode@gmail.com>
2022-07-14 00:30:43 +0100

    add button animations

--- components/Button.vue
+++ components/Button.vue
@@ -16,16 +16,55 @@ export default {
 button {
   border: none;
   padding: 15px 45px;
-  border: 2px solid #a966c8;
+  border: 1px solid #a966c8;
+  box-shadow: inset 0px 0.7px 0px #a966c8;
   font-size: 18px;
-  transition: 0.3s ease-out;
-  background: #a966c8;
+  background: #a966c800;
   color: #ffffff;
+  position: relative;
+  overflow: hidden;
+
+  &::after {
+    content: '';
+    width: 101%;
+    height: 0%;
+    background: #801887;
+    border-radius: 50%;
+    position: absolute;
+    transition: 0.5s ease-out;
+    left: -1px;
+    z-index: -3;
+    bottom: -200%;
+  }
+
+  &::before {
+    content: '';
+    width: 121%;
+    height: 400%;
+    background: #a966c8;
+    position: absolute;
+    left: -20px;
+    bottom: -200%;
+    z-index: -3;
+  }
 
   &:hover {
-    background: #8e49ae;
-    transition: 0.3s ease-out;
-    color: #ffffff;
+    color: #a966c8;
+    transition: 0.5s ease-in;
+    box-shadow: none;
+
+    &::after {
+      content: '';
+      width: 101%;
+      height: 490%;
+      background: rgb(253, 239, 255);
+      border-radius: 40%;
+      position: absolute;
+      left: -1px;
+      bottom: -200%;
+      transition: 0.5s ease-in;
+      z-index: -1;
+    }
   }
 }
 </style>
