c188755f86d941ef2b7fc4c2a468f767eb052518
Sam-kayode <marvelloussamkayode@gmail.com>
2022-07-14 00:58:05 +0100

    add button animations

--- components/Button.vue
+++ components/Button.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <button>
+    <button class="sq-btn">
       <slot></slot>
     </button>
   </div>
@@ -13,11 +13,11 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-button {
-  border: none;
+.sq-btn {
+  border: none ;
   padding: 15px 45px;
   border: 1px solid #a966c8;
-  box-shadow: inset 0px 0.7px 0px #a966c8;
+  box-shadow: inset 1px 1px 1px #a966c8;
   font-size: 18px;
   background: #a966c800;
   color: #ffffff;
@@ -28,7 +28,7 @@ button {
     content: '';
     width: 101%;
     height: 0%;
-    background: #801887;
+    background: #801887 ;
     border-radius: 50%;
     position: absolute;
     transition: 0.5s ease-out;
@@ -39,9 +39,9 @@ button {
 
   &::before {
     content: '';
-    width: 121%;
+    width: 121% ;
     height: 400%;
-    background: #a966c8;
+    background: #a966c8 ;
     position: absolute;
     left: -20px;
     bottom: -200%;
@@ -54,16 +54,16 @@ button {
     box-shadow: none;
 
     &::after {
-      content: '';
-      width: 101%;
-      height: 490%;
-      background: rgb(253, 239, 255);
-      border-radius: 40%;
-      position: absolute;
-      left: -1px;
-      bottom: -200%;
-      transition: 0.5s ease-in;
-      z-index: -1;
+      content: '' ;
+      width: 101% ;
+      height: 490% ;
+      background: rgb(253, 239, 255) ;
+      border-radius: 40% ;
+      position: absolute ;
+      left: -1px ;
+      bottom: -200% ;
+      transition: 0.5s ease-in ;
+      z-index: -1 ;
     }
   }
 }
