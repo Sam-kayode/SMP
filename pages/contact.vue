e8bfdc8189bc91234eca4669e957ce961bd942bc
Sam-kayode <marvelloussamkayode@gmail.com>
2022-07-20 02:10:45 +0100

    add contactanimation

---
+++ pages/contact.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="services"><img src="~/static/images/anim.gif" alt="" /></div>
+</template>
+
+<script>
+export default {
+  name: 'Contact',
+  layout:"none"
+}
+</script>
+
+<style lang="scss" scoped>
+.services {
+  overflow:clip;
+  height:90vh;
+  display:flex;
+  align-items: center;
+  justify-content: center;
+  img {
+    width: 100vw;
+    
+  }
+}
+</style>
