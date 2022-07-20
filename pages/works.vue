d485114eeb4ce12560f1a067157c3fd2780b1d4e
Sam-kayode <marvelloussamkayode@gmail.com>
2022-07-20 02:08:25 +0100

    add aimation to works

--- pages/works.vue
+++ pages/works.vue
@@ -1,11 +1,24 @@
 <template>
-  <div></div>
+  <div class="services"><img src="~/static/images/anim.gif" alt="" /></div>
 </template>
 
 <script>
 export default {
   name: 'Works',
+  layout:"none"
 }
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.services {
+  overflow:clip;
+  height:90vh;
+  display:flex;
+  align-items: center;
+  justify-content: center;
+  img {
+    width: 100vw;
+    
+  }
+}
+</style>
