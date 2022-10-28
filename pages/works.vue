dbb0831e9ed645011e77c216030f09e8113742b9
Sam-kayode <marvelloussamkayode@gmail.com>
2022-10-28 07:14:56 +0100

    add alignment adjustments

--- pages/works.vue
+++ pages/works.vue
@@ -322,6 +322,9 @@ export default {
         }
 
         .graphic {
+          display:flex;
+          align-items: center;
+          justify-content: center;
           img{
           width:70%;
           }
